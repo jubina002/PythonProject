bd128871d6a26205ec050fb6557b1bdfe8ccd62e
jubina002 <70877578+jubina002@users.noreply.github.com>
2024-03-04 14:45:05 +0545

    Update and rename multiplemail.py to sendingmail.py

--- sendingmail.py
+++ sendingmail.py
@@ -2,7 +2,10 @@ import smtplib as s
 
 obj = s.SMTP("smtp.gmail.com", 587)
 obj.ehlo()
+#EHLO ("Extended Hello") is the SMTP command the client uses to tell the server that it is an SMTP client
 obj.starttls()
+# STARTTLS is a protocol command that tells the email server that the other party (email server or client)
+# wants to switch from an insecure plain text connection to a secure connection using TLS or SSL
 obj.login("yourmail@gmail.com", "pwww")
 subject = "Testing Python"
 body= "Hey!! what's up"
