a2e2ca852bef3709b43fbb31095940c0a1920e7c
jubina002 <70877578+jubina002@users.noreply.github.com>
2024-03-09 10:27:57 +0545

    Delete splitPdf.py

--- splitPdf.py
+++
@@ -1,8 +0,0 @@
-import pikepdf
-
-old_pdf = pikepdf.Pdf.open("pm.pdf")
-for n, page_content in enumerate(old_pdf.pages):
-    new_pdf = pikepdf.Pdf.new()
-    new_pdf.pages.append(page_content)
-    name = "test"+str(n)+".pdf"
-    new_pdf.save(name)
