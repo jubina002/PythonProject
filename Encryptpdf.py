eccf448b0ea3a5eacc3faf9de0acd8ff1d19d441
jubina002 <70877578+jubina002@users.noreply.github.com>
2024-03-09 10:26:30 +0545

    Update Encryptpdf.py

--- Encryptpdf.py
+++ Encryptpdf.py
@@ -1,8 +1,2 @@
-import pikepdf
-old_pdf = pikepdf.Pdf.open("pm.pdf")
-no_extr = pikepdf.Permissions(extract=False)
-old_pdf.save("EncryptedPdf.pdf", 
-             encryption= pikepdf.Encryption(user="noneofyobusiness",
-                                            owner="me",
-                                            allow=no_extr))
+
 
